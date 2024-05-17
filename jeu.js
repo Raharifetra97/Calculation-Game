526c87caedee1922d721262df91dc504e5b28158
Mosheh <moiseraharifetra@gmail.com>
2024-05-17 10:47:05 +0200

    Ajoute de fonction qui génére l_opérateur et les nombres aléatoire. Et une nouvelle méthode qui affiche une message lorsque le champ est vide.

--- jeu.js
+++ jeu.js
@@ -1,53 +1,68 @@
-var nb1 = document.querySelector('.nb1')
-var nb2 = document.querySelector('.nb2')
-var operateur = document.querySelector('.oper')
-var Message = document.querySelector('.Message')
-var score = document.querySelector('.score')
-var link = document.querySelector('.link')
-var section = document.querySelector('section')
-var compteur = 0
-var opera = ['+', '-', '*']
 
+document.addEventListener('DOMContentLoaded', () => {
+    var nb1 = document.querySelector('.nb1');
+    var nb2 = document.querySelector('.nb2');
+    var operateur = document.querySelector('.oper');
+    var message = document.querySelector('.Message');
+    var score = document.querySelector('.score');
+    var link = document.querySelector('.link');
+    var section = document.querySelector('section');
+    var compteur = 0;
+    var opera = ['+', '-', '*'];
+    var random1, random2, operaRandom;
 
-//Nombres Aléatoires début du jeu
-random1 = Math.random()*101 << 0
-random2 = Math.random()*101 << 0
-//Insérer les nombres au hasard dans les variables nb1 et nb2
-nb1.innerHTML = random1
-nb2.innerHTML = random2
-
-//Générer des nombres aléatoires entre 1 et 4
-operaRandom = Math.floor(Math.random()*3) + 1
-console.log(operaRandom-1);
-console.log(typeof(opera[operaRandom-1]));
-operateur.innerHTML = opera[operaRandom-1]
-
-// Fonction de vérification du résultat
-function verifier() {
-    // Récuperer le résultat entré par l'utilisateur
-    var resultat = document.querySelector('.resultat').value
-    if ((opera[operaRandom-1] == '+' && random1 + random2 == resultat) || (opera[operaRandom-1] == '-' && random1-random2 == resultat) || (opera[operaRandom-1] == '*' && random1*random2 == resultat)) {
-        Message.style.background = "green"
-        Message.innerHTML = "Correcte"
-        //Nombres Aléatoires début du jeu
-        random1 = Math.random()*101 << 0
-        random2 = Math.random()*101 << 0
-        //Insérer les nombres au hasard dans les variables nb1 et nb2
-        nb1.innerHTML = random1
-        nb2.innerHTML = random2
-        resultat.innerHTML = ""
-        compteur = compteur + 1
-        operaRandom = Math.floor(Math.random()*3) + 1
-        operateur.innerHTML = opera[operaRandom-1]
+    // Fonction pour générer des nombres et un opérateur aléatoires
+    function genererCalcul() {
+        random1 = Math.floor(Math.random() * 101);
+        random2 = Math.floor(Math.random() * 101);
+        operaRandom = Math.floor(Math.random() * 3);
+        
+        nb1.innerHTML = random1;
+        nb2.innerHTML = random2;
+        operateur.innerHTML = opera[operaRandom];
     }
-    /* else if (operaRandom == 1 && random1-random2 == resultat) {
 
-    } */
-    else{
-        Message.style.background = "red"
-        Message.innerHTML = "Vous avez perdu"
-        section.innerHTML = ""
-        score.innerHTML = `Score Final <i class="fa fa-smile-o"></i>  </br> <span>${compteur}</span> `
-        link.style.display = "block"
-    }
-}
+    // Appel initial pour générer un calcul au chargement de la page
+    genererCalcul();
+
+    // Fonction de vérification du résultat
+    window.verifier = function() {
+        var resultat = parseInt(document.querySelector('.resultat').value);
+
+        // Vérification que le résultat entré est un nombre
+        if (isNaN(resultat)) {
+            message.style.background = "orange";
+            message.innerHTML = "Veuillez entrer un nombre.";
+            return;
+        }
+
+        // Vérification du calcul
+        var correct;
+        if (opera[operaRandom] == '+') {
+            correct = (random1 + random2 === resultat);
+        } else if (opera[operaRandom] == '-') {
+            correct = (random1 - random2 === resultat);
+        } else if (opera[operaRandom] == '*') {
+            correct = (random1 * random2 === resultat);
+        }
+
+        if (correct) {
+            message.style.background = "green";
+            message.innerHTML = "Correcte";
+            compteur++;
+            score.innerHTML = `Score: ${compteur}`;
+        } else {
+            message.style.background = "red";
+            message.innerHTML = "Vous avez perdu";
+            section.innerHTML = "";
+            score.innerHTML = `Score Final <i class="fa fa-smile-o"></i> <br> <span>${compteur}</span>`;
+            link.style.display = "block";
+            return;
+        }
+
+        // Générer un nouveau calcul
+        genererCalcul();
+        // Réinitialiser le champ de résultat
+        document.querySelector('.resultat').value = "";
+    };
+});
